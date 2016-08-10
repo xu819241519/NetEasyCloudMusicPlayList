635dfbd488ca52f30aca052bfb9eb32e227c678f
landon <819241519@qq.com>
2016-08-10 15:54:38 +0800

    添加部分显示功能
    待解决的问题：
    1、swiperefresh实现上拉刷新
    2、gridview换成recycleview
    3、gridview item换成cardview
    4、添加网络刷新按钮
    5、补充完整爬取信息
    6、链接网易云音乐，反编译

--- app/src/main/java/com/landon/neteasycloudmusicplaylist/adapter/PlayListAdapter.java
+++ app/src/main/java/com/landon/neteasycloudmusicplaylist/adapter/PlayListAdapter.java
@@ -1,9 +1,15 @@
 package com.landon.neteasycloudmusicplaylist.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.landon.neteasycloudmusicplaylist.R;
 import com.landon.neteasycloudmusicplaylist.bean.PlayListBean;
 
 import java.util.ArrayList;
@@ -14,11 +20,19 @@ import java.util.List;
  */
 public class PlayListAdapter extends BaseAdapter {
 
+    private Context context;
+
+    public PlayListAdapter(Context context) {
+        this.context = context;
+    }
+
     private List<PlayListBean> playListBeanList = new ArrayList<>();
 
     @Override
     public int getCount() {
-        return playListBeanList.size();
+        if (playListBeanList == null || playListBeanList.size() == 0)
+            return 1;
+        else return playListBeanList.size();
     }
 
     @Override
@@ -33,15 +47,42 @@ public class PlayListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            if (playListBeanList == null || playListBeanList.size() == 0) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.item_no_playlist, null);
+            } else {
+                convertView = LayoutInflater.from(context).inflate(R.layout.item_playlist, null);
+                viewHolder = new ViewHolder();
+                viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.iv_image);
+                viewHolder.tvName = (TextView) convertView.findViewById(R.id.tv_name);
+                convertView.setTag(viewHolder);
+            }
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        if (playListBeanList != null && playListBeanList.size() > 0) {
+
+//            Glide.with(context).load(playListBeanList.get(position).getImage()).into(viewHolder.ivImage);
+            if(playListBeanList.get(position).getName() != null && viewHolder != null && viewHolder.tvName != null)
+                viewHolder.tvName.setText(playListBeanList.get(position).getName());
+        }
+        return convertView;
     }
 
     /**
      * 更新数据
+     *
      * @param beans 歌单数据
      */
-    public void update(List<PlayListBean> beans){
+    public void update(List<PlayListBean> beans) {
         playListBeanList = beans;
         notifyDataSetChanged();
+    }
+
+    public static class ViewHolder {
+        public ImageView ivImage;
+        public TextView tvName;
     }
 }
--- app/src/main/java/com/landon/neteasycloudmusicplaylist/database/CrawlerSQLHelper.java
+++ app/src/main/java/com/landon/neteasycloudmusicplaylist/database/CrawlerSQLHelper.java
@@ -33,7 +33,7 @@ public class CrawlerSQLHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS" + databaseName +" (id integer primary key,name varchar(500), author varchar(500), url varchar(500)), image varchar(500)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + databaseName + " (id integer primary key,name varchar(500), author varchar(500), url varchar(500), image varchar(500))");
     }
 
     @Override
@@ -43,79 +43,99 @@ public class CrawlerSQLHelper extends SQLiteOpenHelper {
 
     /**
      * 插入一个歌单
+     *
      * @param bean 歌单bean
      * @return 成功插入返回true
      */
-    public boolean insert(PlayListBean bean){
+    public boolean insert(PlayListBean bean) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put("name",bean.getName());
-        cv.put("id",bean.getId());
-        cv.put("author",bean.getAuthor());
-        cv.put("image",bean.getImage());
-        cv.put("url",bean.getUrl());
+        cv.put("name", bean.getName());
+        cv.put("id", bean.getId());
+        cv.put("author", bean.getAuthor());
+        cv.put("image", bean.getImage());
+        cv.put("url", bean.getUrl());
         boolean result = db.insert(databaseName, null, cv) != -1;
 //        db.close();
         return result;
     }
 
     /**
+     * 插入歌单
+     *
+     * @param beans
+     * @return
+     */
+    public boolean insert(List<PlayListBean> beans) {
+        if (beans != null && beans.size() > 0) {
+            for (int i = 0; i < beans.size(); ++i) {
+                insert(beans.get(i));
+            }
+        }
+        return true;
+    }
+
+    /**
      * 删除数据中所有的歌单
+     *
      * @return 删除成功返回true
      */
-    public boolean deleteAll(){
+    public boolean deleteAll() {
         SQLiteDatabase db = getWritableDatabase();
-        db.delete(databaseName,null,null);
+        db.delete(databaseName, null, null);
 //        db.close();
         return true;
     }
 
     /**
      * 更新指定歌单
+     *
      * @param bean 要更新的歌单
      * @return 更新成功返回true
      */
-    public boolean update(PlayListBean bean){
+    public boolean update(PlayListBean bean) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put("name",bean.getName());
-        cv.put("author",bean.getAuthor());
-        cv.put("image",bean.getImage());
-        cv.put("url",bean.getUrl());
-        boolean result =  db.update(databaseName,cv,"id = ?",new String[]{String.format(Locale.CHINA,"%d",bean.getId())}) > 0;
+        cv.put("name", bean.getName());
+        cv.put("author", bean.getAuthor());
+        cv.put("image", bean.getImage());
+        cv.put("url", bean.getUrl());
+        boolean result = db.update(databaseName, cv, "id = ?", new String[]{String.format(Locale.CHINA, "%d", bean.getId())}) > 0;
 //        db.close();
         return result;
     }
 
     /**
      * 删除歌单
+     *
      * @param bean 要删除的歌单
      * @return 删除成功返回true
      */
-    public boolean delete(PlayListBean bean){
+    public boolean delete(PlayListBean bean) {
         SQLiteDatabase db = getWritableDatabase();
-        boolean result = db.delete(databaseName,"id = ?", new String[]{String.format(Locale.CHINA,"%d",bean.getId())}) > 0;
+        boolean result = db.delete(databaseName, "id = ?", new String[]{String.format(Locale.CHINA, "%d", bean.getId())}) > 0;
 //        db.close();
         return result;
     }
 
     /**
      * 查询歌单
+     *
      * @param id
      * @return
      */
-    public PlayListBean query(long id){
+    public PlayListBean query(long id) {
         PlayListBean bean = new PlayListBean();
         SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.query(databaseName,null,"id = ?",new String[]{String.format(Locale.CHINA,"%d",id)},null,null,null);
-        if(cursor == null){
+        Cursor cursor = db.query(databaseName, null, "id = ?", new String[]{String.format(Locale.CHINA, "%d", id)}, null, null, null);
+        if (cursor == null) {
             return bean;
         }
-        if(cursor.getCount() == 0){
+        if (cursor.getCount() == 0) {
             cursor.close();
             return bean;
         }
-        if(cursor.moveToNext()){
+        if (cursor.moveToNext()) {
             bean.setId(cursor.getInt(0));
             bean.setName(cursor.getString(1));
             bean.setAuthor(cursor.getString(2));
@@ -129,20 +149,21 @@ public class CrawlerSQLHelper extends SQLiteOpenHelper {
 
     /**
      * 查询全部歌单
+     *
      * @return
      */
-    public List<PlayListBean> query(){
+    public List<PlayListBean> query() {
         List<PlayListBean> beans = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.query(databaseName,null,null,null,null,null,null);
-        if(cursor == null){
+        Cursor cursor = db.query(databaseName, null, null, null, null, null, null);
+        if (cursor == null) {
             return beans;
         }
-        if(cursor.getCount() == 0){
+        if (cursor.getCount() == 0) {
             cursor.close();
             return beans;
         }
-        while(cursor.moveToNext()){
+        while (cursor.moveToNext()) {
             PlayListBean bean = new PlayListBean();
             bean.setId(cursor.getInt(0));
             bean.setName(cursor.getString(1));
@@ -157,16 +178,17 @@ public class CrawlerSQLHelper extends SQLiteOpenHelper {
     }
 
 
-    public void close(){
+    public void close() {
         getWritableDatabase().close();
         getReadableDatabase().close();
     }
 
     /**
      * 异步查询
+     *
      * @param handler
      */
-    public void queryAsync(final Handler handler){
+    public void queryAsync(final Handler handler) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -176,5 +198,36 @@ public class CrawlerSQLHelper extends SQLiteOpenHelper {
                 msg.sendToTarget();
             }
         }).start();
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public List<PlayListBean> query(int page, int pageSize) {
+        List<PlayListBean> beans = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + databaseName + " order by id limit " +  String.format(Locale.CHINA, "%d", page * pageSize) + "," + String.format(Locale.CHINA, "%d", pageSize),null);
+        if (cursor == null) {
+            return beans;
+        }
+        if (cursor.getCount() == 0) {
+            cursor.close();
+            return beans;
+        }
+        while (cursor.moveToNext()) {
+            PlayListBean bean = new PlayListBean();
+            bean.setId(cursor.getInt(0));
+            bean.setName(cursor.getString(1));
+            bean.setAuthor(cursor.getString(2));
+            bean.setUrl(cursor.getString(3));
+            bean.setImage(cursor.getString(4));
+            beans.add(bean);
+        }
+        cursor.close();
+//        db.close();
+        return beans;
     }
 }
